17e95488523866be0cba62eebd7e0c92a90e8e05
23wh1a0521 <23wh1a0521@bvrithyderabad.edu.in>
2026-04-10 18:10:12 +0530

    routing app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-
+import QuestionPage from './pages/QuestionPage';
 function App() {
   return (
-    <div>
-      <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/tasks" replace />} />
+        <Route path="/tasks" element={<Home />} />
+        <Route path="/question" element={<QuestionPage />} />
+      </Routes>
 
       {/* TODO: Replace this placeholder with Routes */}
-    </div>
+    </BrowserRouter>
   );
 }
 
